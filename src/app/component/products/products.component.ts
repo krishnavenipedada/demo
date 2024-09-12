beee0802356f4e6c98e19dd8f908903f93a7a956
krishnavenipedada <128021276+krishnavenipedada@users.noreply.github.com>
2024-09-12 11:18:28 +0530

    Add files via upload

---
+++ src/app/component/products/products.component.ts
@@ -0,0 +1,43 @@
+import { Component,OnInit } from '@angular/core';
+import { ApiService } from '../../service/api.service';
+import { CartService } from '../../service/cart.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrl:'./products.component.css'
+})
+export class ProductsComponent implements OnInit{
+  public productList: any;
+  public filterCategory:any
+  searchKey: string = "";
+  constructor(private api:ApiService,private cartService:CartService) { }
+    ngOnInit(): void {
+      this.api.getProduct()
+        .subscribe(res => {
+          this.productList = res;
+          this.filterCategory = res;
+          this.productList.forEach((a: any) => {
+          
+            Object.assign(a, { quantity: 1, total: a.price });
+            
+          });
+        });
+      this.cartService.search.subscribe((val:any) => {
+        this.searchKey = val;
+      })
+    
+  }
+  addtocart(item:any) {
+    this.cartService.addtoCart(item);
+  }
+  filter(category: string) {
+    this.filterCategory = this.productList
+      .filter((a: any)=> {
+        if (a.category == category || category == '') {
+          return a;
+      }
+    })
+  }
+
+}
